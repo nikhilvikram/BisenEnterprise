438acf934e9b5092443c9b98d5bbe7213784d69c
Nikhil Bisen <nikhilbisen25@gmail.com>
2025-12-03 19:57:10 +0530

    Fix Port for Render and clean CORS

--- Backend/server.js
+++ Backend/server.js
@@ -33,5 +33,5 @@ app.use("/api/auth", authRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/wishlist", wishlistRoutes);
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;;
 app.listen(PORT, () => console.log(`✔ Server running on port ${PORT}`));
