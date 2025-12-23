a82876f7df6f26619822806e4be907ff5294f914
Nikhil Bisen <nikhilbisen25@gmail.com>
2025-12-23 22:34:35 +0530

    Bought bisenenterprise.com url3

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -21,8 +21,9 @@ import react from "@vitejs/plugin-react";
 // - "/BisenEnterprise/" on GitHub Pages
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production"
-    ? "/BisenEnterprise/"
-    : "/",
+  // base: process.env.NODE_ENV === "production"
+  //   ? "/BisenEnterprise/"
+  //   : "/",
+  base: "/"
 });
 
