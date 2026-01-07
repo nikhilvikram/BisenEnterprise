1d0239549c7b017a0223914b835ef7519c6f7650
Nikhil Bisen <nikhilbisen25@gmail.com>
2026-01-08 00:13:13 +0530

    Fix: Change API_URL to relative path to solve Mixed Content

--- Frontend/src/config.jsx
+++ Frontend/src/config.jsx
@@ -6,7 +6,7 @@
 // If we are running "npm run build" (Live), use AWS EC2.
 
 export const API_URL = import.meta.env.MODE === "production"
-  ? "http://3.110.221.128/api"   // ☁️ Your New AWS Server
+  ? "/api"   // ☁️ Your New AWS Server
   : "http://localhost:5000/api";      // 💻 Your Local Computer
 
 // export const API_URL = "http://3.110.221.128:5000/api";
